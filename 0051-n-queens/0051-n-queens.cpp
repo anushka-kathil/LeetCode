dbd8ea8676153f816b69dbd6f40c70a7786b906a
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-02-28 04:05:55 +0530

    Time: 0 ms (100%), Space: 10 MB (92.73%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool isSafe(int col,int row,  vector<string>& board,int n){
+        // No need to check for upper and lower board[row][col]
+        int duprow=row;
+        int dupcol=col;
+       // Checking left upper diagonal
+        while(row>=0 && col>=0){
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+        
+        // left side
+        row=duprow;
+        col=dupcol;
+        while(col>=0){
+            if(board[row][col] == 'Q') return false;
+            col--;
+        }
+      
+       
+        // checking lower left diagonal
+        row=duprow;
+        col=dupcol;
+        while(row<n && col>=0){
+            if(board[row][col] == 'Q') return false;
+            row++;
+            col--;
+        }
+        return true;
+    }
+
+public:
+    void solve(int col,int n, vector<vector<string>>& ans, vector<string>& board){
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row=0;row<n;row++){
+            if(isSafe(col,row,board,n)){
+                board[row][col]='Q';
+                solve(col+1,n,ans,board);
+                board[row][col]='.';
+            }
+        }
+       
+    }
+
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        solve(0,n,ans,board);
+        return ans;
+        
+    }
+};
