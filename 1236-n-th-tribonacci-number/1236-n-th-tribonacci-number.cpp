5c2ac038427db0d662d61009641521a079a09b13
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-03-01 15:08:10 +0530

    Time: 0 ms (100%), Space: 8.1 MB (41.71%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -9,16 +9,17 @@ public:
 
         if(n==2)
         return 1;
+        
         vector<int> v;
         v.push_back(0);
         v.push_back(1);
         v.push_back(1);
         v.push_back(2);
 
- 
         if(n==3){
             return 0+1+1;
         }
+
        long long sum=2;
        int pointer=0;
 
@@ -30,20 +31,7 @@ public:
         }
         return sum;
 
-        // int s=0,s1=1,s2=1;
-        // int sum=s+s1+s2;
-        
-        // for(int i=3;i<)
-
-        // while(n>2){
-
-
-        // }
-       
-        // return n;
-        
-
-        // return tribonacci (n-1 + n-2 + n-3);
+      
         
     }
 };
