31dd36b8522c2c9ff47aada5fc9f0c2f9f87ac75
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-03-02 23:20:02 +0530

    Time: 0 ms (100%), Space: 8.4 MB (92.63%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+
+    double myPow(double x, int n) {
+       
+        // if()
+        long double result=pow(x,n);
+        //  if(n==n/2)
+        //  return n;
+
+        // if(n>0)
+        // {
+        // long double ans=myPow(x,n-1);
+        
+        // long double result=x*ans;
+
+        return result; 
+        // }
+
+        // else{
+        //    long double ans=myPow(x,n+1);
+        //    long double result=(1/x)*ans;
+
+            // return result; 
+
+        
+
+    }
+};
