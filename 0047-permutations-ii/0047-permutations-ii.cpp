c7b95d1fd87304d873314e2b635c82dd8cfd51b1
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-03-07 23:43:30 +0530

    Time: 1767 ms (5.03%), Space: 285.8 MB (5.11%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    void permutation(vector<int>& nums, vector<int>& v, vector<vector<int>>& ans,unordered_map<int,bool> m){ 
+   bool isPresent = std::find(ans.begin(), ans.end(), v) != ans.end();
+    if(isPresent)
+    return; 
+
+    if(v.size()==nums.size()){
+        ans.push_back(v);
+        return;
+    }
+
+    for(int i=0;i<nums.size();i++ ){
+        if(m[i]==false ){
+            v.push_back(nums[i]);
+            m[i]=true;
+            permutation(nums,v,ans,m);
+            v.pop_back();
+            m[i]=false;
+        }
+
+    }
+   }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+         vector<int> v;
+        vector<vector<int>> ans;
+        unordered_map<int,bool> m;
+
+        permutation(nums,v,ans,m);
+        return ans;
+        
+        
+        
+    }
+};
