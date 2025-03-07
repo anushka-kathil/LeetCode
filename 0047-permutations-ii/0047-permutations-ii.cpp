440bddac51fe45501bf749d12b6df2a6a8f1fd1e
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-03-08 00:37:23 +0530

    Time: 0 ms (100%), Space: 11.6 MB (65.13%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
 
-    void permutation(vector<int>& nums, vector<int>& v, vector<vector<int>>& ans,unordered_map<int,bool> m){ 
-   bool isPresent = std::find(ans.begin(), ans.end(), v) != ans.end();
-    if(isPresent)
-    return; 
-
+    void permutation(vector<int>& nums, vector<int>& v, vector<vector<int>>& ans,unordered_map<int,bool>& m){ 
     if(v.size()==nums.size()){
         ans.push_back(v);
         return;
     }
 
     for(int i=0;i<nums.size();i++ ){
+        if(m[i] == true) continue;
+        if (i > 0 && nums[i] == nums[i - 1] && m[i - 1] == false) continue;
         
-
-        if (i > 0 && nums[i] == nums[i - 1] && m[i - 1] == true) continue;
-        if(m[i]==false ){
             v.push_back(nums[i]);
             m[i]=true;
             permutation(nums,v,ans,m);
             v.pop_back();
             m[i]=false;
-        }
 
     }
    }
@@ -29,6 +23,7 @@ public:
          vector<int> v;
         vector<vector<int>> ans;
         unordered_map<int,bool> m;
+        sort(nums.begin(),nums.end());
 
         permutation(nums,v,ans,m);
         return ans;
