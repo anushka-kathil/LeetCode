4ea8240c9c51afc4c3f5403b862cc0850a785671
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-03-12 20:50:00 +0530

    Time: 0 ms (100%), Space: 8.4 MB (36.96%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int digit(int n){
+        int ans=0;
+        while(n){
+            int d=n%10;
+            ans+=(d*d);
+            n=n/10;
+        }
+        return ans;
+    }
+    bool isHappy(int n) {
+        if(n==1)
+        return true;
+
+       int ans=INT_MAX,c=0;
+       unordered_map <int,int> m;
+        while(m[n] == 0 && n!=1){
+            m[n]++;
+            int ans=digit(n);
+            n=ans;
+            if(n==1){
+                c++;
+                break;
+            }
+            
+        }
+        if(c!=0)
+        return true;
+
+        return false;
+             
+    }
+};
