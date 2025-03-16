b1e23aaf55b1c9f2fc51ea4e45316348fd03bf93
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-03-17 00:06:29 +0530

    Time: 0 ms (100%), Space: 14.3 MB (74.49%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -4,8 +4,8 @@ public:
          vector<int> ans;
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
-        int n1=nums1.size(),n2=nums2.size();
-        int i=0,j=0;
+        int n1=nums1.size(),n2=nums2.size(),i=0,j=0;;
+   
         while(i<n1 && j<n2){
             if(nums1[i]==nums2[j]){
             ans.push_back(nums1[i]);
