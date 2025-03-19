9362a55931bf696e211541d14d9b5d84874976c2
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-03-19 20:40:48 +0530

    Time: 0 ms (100%), Space: 11.5 MB (84.89%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -9,28 +9,18 @@ public:
         return 1;
         int hash[255];
         memset(hash, -1, sizeof(hash));
-        // int arr[5]={}
-        // for(int i=0;i<255;i++){
-        //     hash[i]=-1;
-        // }
         
         int l=0,r=0,index=0;
         int len=0,maxLen=0;
-        // cout<<"->"<<hash[s[index]]<<endl;
-        // cout<<"->"<<hash['i']<<endl;
          
-        
-
         while(r<n){
             if(hash[s[index]] == -1){
-                // cout<<"*"<<endl;
                 len = r-l+1;
                 maxLen=max(maxLen , len);
                 hash[s[index]]=index;
                 r++;
             }
             else if(l <= hash[s[index]]){
-                //  cout<<"&"<<endl;
                 l=hash[s[index]] + 1;
                 hash[s[index]]= index;
                 len = r-l+1;
@@ -38,14 +28,12 @@ public:
                 r++;
             }
             else{
-                //  cout<<"#"<<endl;
                 hash[s[index]]= index;
                 len = r-l+1;
                 maxLen=max(maxLen , len);
                 r++;
             }
             index++;
-            //  cout<<"**"<<endl;
         }
         return maxLen;
     }
