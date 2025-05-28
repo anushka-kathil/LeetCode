2fbe7500c0827d9da84310d06672bfe323f2aa6f
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-05-28 17:58:01 +0530

    Time: 0 ms (100%), Space: 15 MB (62.8%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,38 +11,41 @@
  */
 class Solution {
 public:
-    // void check(TreeNode* r, TreeNode* l,vector<int>& v){
-    //    if(!r && !l) return;
-    //    if(r!= NULL){
-    //     v.push_back(r->val);
-    //     if(r->right != NULL && l->)
-    //     check()
-
-    //     else if(r->)
-    //    }
-
-    // }
+// METHOD 2
+   void level(TreeNode* node, vector<int>& v, int l){
+       if(node == NULL) return;
+       if(v.size() == l)
+         v.push_back(node->val);
+        
+        level(node->right,v,l+1);
+        level(node->left,v,l+1);
+        
+   }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> v;
-        if(!root) return v;
-        // check(root->right,root->left,v);
-        queue<TreeNode*> q;
-        q.push(root);
-        
-        while(q.size()){
-            int size = q.size();
-            vector<int> level;
-            for(int i=0;i<size;i++){
-             TreeNode* node = q.front();
-             q.pop();
-
-             level.push_back(node->val);
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-            }
-            v.push_back(level[level.size()-1]);
-        }
+        level(root,v,0);
         return v;
+
+        // METHOD 1
+        // if(!root) return v;
+        
+        // queue<TreeNode*> q;
+        // q.push(root);
+        
+        // while(q.size()){
+        //     int size = q.size();
+        //     vector<int> level;
+        //     for(int i=0;i<size;i++){
+        //      TreeNode* node = q.front();
+        //      q.pop();
+
+        //      level.push_back(node->val);
+        //         if(node->left) q.push(node->left);
+        //         if(node->right) q.push(node->right);
+        //     }
+        //     v.push_back(level[level.size()-1]);
+        // }
+        // return v;
         
     }
 };
