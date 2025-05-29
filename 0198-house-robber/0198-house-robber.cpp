e22b96a75a50aa106260301351f73b25d610a572
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-05-30 01:49:18 +0530

    Time: 0 ms (100%), Space: 10.2 MB (79.41%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    int sol(vector<int>& nums, vector<int>& dp,int index){
-      if(index == 0) return nums[index];
-      if(index < 0) return 0;
-      if(dp[index] != -1) return dp[index];
+// M-1 memorization
+    // int sol(vector<int>& nums, vector<int>& dp,int index){
+    //   if(index == 0) return nums[index];
+    //   if(index < 0) return 0;
+    //   if(dp[index] != -1) return dp[index];
 
-      int pick = nums[index] + sol(nums,dp,index-2);
-      int notpick= 0+sol(nums,dp,index-1);
-      return dp[index]=max(pick,notpick);
-    }
+    //   int pick = nums[index] + sol(nums,dp,index-2);
+    //   int notpick= 0+sol(nums,dp,index-1);
+    //   return dp[index]=max(pick,notpick);
+    // }
 
     int rob(vector<int>& nums) {
+        // M-1
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return sol(nums,dp,n-1);
+        // vector<int> dp(n,-1);
+        // return sol(nums,dp,n-1);
+
+        // M-2 with constant space
+        int prev=nums[0];
+        int prev2 = 0;
+        for(int i=1;i<n;i++){
+            int take = nums[i];
+            if(i>1) take+=prev2;
+
+            int notTake = 0 + prev;
+            int curr = max(take, notTake);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
         
     }
 };
