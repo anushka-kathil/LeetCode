fe43fe25d7945b79d5a28e060074ed4e1ccd1a38
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-06-03 19:39:57 +0530

    Time: 7 ms (35.14%), Space: 18.9 MB (29.35%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+         int N = numCourses;
+        // vector<int> *adj = new vector<int>[N];
+         vector<vector<int>> adj(N);
+        // int adj[numCourses];
+        for(auto it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adj[v].push_back(u);
+        }
+
+        // check for topological sort
+        // int indegree[N];
+        vector<int> indegree(N,0);
+        // int* indegree = new int[N];
+        // vector<int> *indegree = new vector<int>[N];
+        for(int i=0;i<N;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<N;i++){
+            if(indegree[i]==0)
+            q.push(i);
+        }
+        vector<int> topo;
+        while(q.size()){
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+          for(auto it:adj[node]){
+            indegree[it]--;
+            if(indegree[it] == 0) q.push(it);
+          }
+        }
+     if(topo.size() == N)
+      return topo;
+
+     return {};
+    }
+};
