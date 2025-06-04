9422bbe5f75e904adc629f2be626d3607f9ebc89
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-06-04 17:08:27 +0530

    Time: 5 ms (56.5%), Space: 23.8 MB (80.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,33 +1,45 @@
 class Solution {
 
 public:
-    static bool cmp(const vector<int>& a, const vector<int>& b) {
-    return a[0] < b[0];
-    }
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),cmp);
-        vector<vector<int>> merged;
-        vector<int> v ;
-        v.push_back(intervals[0][0]);
-         v.push_back(intervals[0][1]);
-        for(int i=1;i<intervals.size();i++){
-            if(v[1] >= intervals[i][0])
-            {
-                // v.pop_back();
-                if(v[1] <= intervals[i][1]){
-                v.pop_back();
-                v.push_back(intervals[i][1]);
-                }
-            }
-            else{
-                merged.push_back(v);
-                v.clear();
-                v.push_back(intervals[i][0]);
-                v.push_back(intervals[i][1]);
-            }
-        }
-        merged.push_back(v);
-        return merged;
+    // static bool cmp(const vector<int>& a, const vector<int>& b) {
+    // return a[0] < b[0];
+    // }
+vector<vector<int>> merge(vector<vector<int>>& interval) {
+ int n = interval.size();
+ vector<vector<int>> ans;
+sort(interval.begin(),interval.end());
+if(n == 0) return {};
+vector<int> v;
+int last=interval[0][1];
+int small = interval[0][0];
+v.push_back(small);
+v.push_back(last);
+for(int i = 1; i<n;i++){
+if(last >= interval[i][0]) {
+v.clear();
+last = max(last,interval[i][1]);
+small = min(small,interval[i][0]);
+v.push_back(small);
+v.push_back(last);
+}
+else {ans.push_back(v);
+v.clear();
+
+small = interval[i][0];
+last = interval[i][1];
+
+v.push_back(small);
+v.push_back(last);
+}
+}
+if(v.empty() == false) ans.push_back(v);
+if(ans.empty() == true) {
+    // v.push_back(small);
+    // v.push_back(last);
+    ans.push_back(v);
+}
+
+return ans;
         
     }
 };
