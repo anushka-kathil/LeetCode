f3732156af6ca7ae1dcea7fa5ea67a92820d42ee
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-06-19 00:04:26 +0530

    Time: 1 ms (72.05%), Space: 206 MB (99.09%) - LeetHub

--- 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int k=0;
         int prev=-1;
-        for(int i=0; i<nums.size();i++){
+        for(int i=0; i<n;i++){
             if(nums[i] >= prev) {
                 k++;
                 prev=nums[i];
