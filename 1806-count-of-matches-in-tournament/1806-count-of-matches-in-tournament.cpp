fcf95846b290433d019539260197d10fbc9a0f65
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-08-01 01:28:32 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.68%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int carry = 0;
+        int ans=0;
+        while(n>1 || carry == 1){
+            if(n%2 == 0) {
+                ans+=n/2;
+                n=n/2;
+            }
+
+            else{
+              if(carry == 0){
+                n--;
+                carry++;
+              }
+              else{
+                n++;
+                carry--;
+              }
+            }
+        }
+        return ans;  
+    }
+};
