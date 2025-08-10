d92f479ff29933715b63199efd0bc666efe027b8
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-08-11 02:08:24 +0530

    Time: 22 ms (62.64%), Space: 22.6 MB (84.86%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestWord(vector<string>& words) {
+        sort(words.begin(), words.end());          
+        unordered_set<string> built;
+        string best = "";
+
+        for (const string &w : words) {
+            if (w.size() == 1 || built.count(w.substr(0, w.size() - 1))) {
+                built.insert(w);
+                if (w.size() > best.size() ) {
+                    best = w;
+                }
+            }
+        }
+        return best;
+    }
+};
