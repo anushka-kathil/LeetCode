346202dc5c45a6f9da7418bc8c33fdfef30271ce
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-08-12 04:08:17 +0530

    Time: 11 ms (85.03%), Space: 147.7 MB (59.36%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -1,43 +1,27 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
-
-    long long modPow(long long base, long long exp) {
-        long long result = 1;
-        while (exp > 0) {
-            if (exp & 1) result = (result * base) % MOD;
-            base = (base * base) % MOD;
-            exp >>= 1;
-        }
-        return result;
-    }
-
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
-        vector<int> powers;
-        for (int i = 0; i < 31; i++) { 
-            if (n & (1 << i)) {
-                powers.push_back(1 << i);
+        long long i = 0;
+        const long long MOD = 1e9 + 7;
+        vector<long long> powers;
+        vector<int> v;
+
+        while (n) {
+            if (n & 1LL) {
+                powers.push_back(1LL << i);
             }
+            i++;
+            n >>= 1LL;
         }
-        int m = powers.size();
-        vector<long long> prefix(m);
-        prefix[0] = powers[0] % MOD;
-        for (int i = 1; i < m; i++) {
-            prefix[i] = (prefix[i - 1] * powers[i]) % MOD;
-        }
-        vector<int> ans;
-        for (auto &q : queries) {
-            int l = q[0], r = q[1];
-            if (l == 0) {
-                ans.push_back(prefix[r]);
-            } else {
-                long long inv = modPow(prefix[l - 1], MOD - 2); 
-                ans.push_back((prefix[r] * inv) % MOD);
+
+        long long ans = 1;
+        for (auto &query : queries) {
+            ans = 1;
+            for (long long j = query[0]; j <= query[1]; j++) {
+                ans = (ans* powers[j]) % MOD;
             }
+            v.push_back(ans);
         }
-        return ans;
+        return v;
     }
 };
