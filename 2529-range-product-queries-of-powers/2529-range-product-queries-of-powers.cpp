838d33756b0ed8c67a1a71fce6453dcf07489fc7
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-08-11 20:17:01 +0530

    Time: 33 ms (44.15%), Space: 148 MB (37.23%) - LeetHub

---
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    long long modPow(long long base, long long exp) {
+        long long result = 1;
+        while (exp > 0) {
+            if (exp & 1) result = (result * base) % MOD;
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return result;
+    }
+
+    vector<int> productQueries(int n, vector<vector<int>>& queries) {
+        vector<int> powers;
+        for (int i = 0; i < 31; i++) { 
+            if (n & (1 << i)) {
+                powers.push_back(1 << i);
+            }
+        }
+        int m = powers.size();
+        vector<long long> prefix(m);
+        prefix[0] = powers[0] % MOD;
+        for (int i = 1; i < m; i++) {
+            prefix[i] = (prefix[i - 1] * powers[i]) % MOD;
+        }
+        vector<int> ans;
+        for (auto &q : queries) {
+            int l = q[0], r = q[1];
+            if (l == 0) {
+                ans.push_back(prefix[r]);
+            } else {
+                long long inv = modPow(prefix[l - 1], MOD - 2); 
+                ans.push_back((prefix[r] * inv) % MOD);
+            }
+        }
+        return ans;
+    }
+};
