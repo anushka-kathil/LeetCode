2234c09052d8731c54394e28bad2b5c199ef0e7a
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-09-26 00:51:35 +0530

    Time: 0 ms (100%), Space: 12.5 MB (91.62%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    int dfs(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
-        if(i == triangle.size() - 1) return triangle[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int down = dfs(i + 1, j, triangle, dp);
-        int diagonal = dfs(i + 1, j + 1, triangle, dp);
-        
-        dp[i][j] = triangle[i][j] + min(down, diagonal);
-        return dp[i][j];
-    }
-
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        return dfs(0, 0, triangle, dp);
+        vector<int> dp = triangle[n-1];  
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j+1]);
+            }
+        }
+        return dp[0];
     }
 };
