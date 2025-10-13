11a0349350d34138e3c0f2cb8415740e17cc95e6
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2025-10-14 01:05:44 +0530

    Time: 14 ms (7.8%), Space: 21.3 MB (7.66%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s1, string s2){
         unordered_map<char,int> m1;
-        unordered_map<char,int> m2;
         if(s1.size()!=s2.size()) return false;
         for(int i=0; i<s1.size(); i++){
             m1[s1[i]]++;
+            m1[s2[i]]--;
         }
-        for(int i=0; i<s1.size(); i++){
-            if(m1.find(s2[i]) != m1.end()) {
-                m1[s2[i]]--;
-            }
-            if(m1[s2[i]] == 0) m1.erase(s2[i]);
+        for(auto x:m1){
+            if(x.second!=0) return false;
         }
-        if(m1.empty() == true) return true;
-        else return false;
+        return true;
     }
 
     vector<string> removeAnagrams(vector<string>& words) {
