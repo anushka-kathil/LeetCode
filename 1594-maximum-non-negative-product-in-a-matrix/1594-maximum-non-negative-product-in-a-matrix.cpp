fecafa6a6c9da6751c25c21b28ae2c2dd1b0b567
Anushka Kathil <149619819+anushka-kathil@users.noreply.github.com>
2026-03-24 01:56:52 +0530

    Time: 0 ms (100%), Space: 14.1 MB (74.66%) - LeetHub

---
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxProductPath(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        const long long mod = 1e9 + 7;
+
+        vector<vector<long long>> mx(m, vector<long long>(n));
+        vector<vector<long long>> mn(m, vector<long long>(n));
+
+        mx[0][0] = mn[0][0] = grid[0][0];
+
+        for(int i = 1; i < m; i++) {
+            mx[i][0] = mx[i-1][0] * grid[i][0];
+            mn[i][0] = mn[i-1][0] * grid[i][0];
+        }
+
+        for(int j = 1; j < n; j++) {
+            mx[0][j] = mx[0][j-1] * grid[0][j];
+            mn[0][j] = mn[0][j-1] * grid[0][j];
+        }
+
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                long long val = grid[i][j];
+
+                long long a = mx[i-1][j] * val;
+                long long b = mn[i-1][j] * val;
+                long long c = mx[i][j-1] * val;
+                long long d = mn[i][j-1] * val;
+
+                mx[i][j] = max({a, b, c, d});
+                mn[i][j] = min({a, b, c, d});
+            }
+        }
+
+        if(mx[m-1][n-1] < 0) return -1;
+        return mx[m-1][n-1] % mod;
+    }
+};
